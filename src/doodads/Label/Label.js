f4450fe931fb66b1aa05d8a1b620486ce79ff154
Sahab Yazdani <sahab.yazdani+github@gmail.com>
2012-02-23 09:54:30 -0500

    jQueryClosure

--- src/doodads/Label/Label.js
+++ src/doodads/Label/Label.js
@@ -1,22 +1,24 @@
-﻿doodads.setup().inherits()(function(base) {
-	this.constructor(function () {
-		this._text = this._options.text;
-	})
-	.defaultOptions({
-		text: ''
-	})
-	.proto({
-		templateData: function Label$templateData() {
-			return { text: this._text };
-		},
-		text: function Label$text(/*text*/) {
-			if (arguments.length === 0) {
-				return this._text;
-			} else {
-				this._text = arguments[0];
-				this.element().text(this._text);
+﻿(function($) {
+	doodads.setup().inherits()(function(base) {
+		this.constructor(function () {
+			this._text = this._options.text;
+		})
+		.defaultOptions({
+			text: ''
+		})
+		.proto({
+			templateData: function Label$templateData() {
+				return { text: this._text };
+			},
+			text: function Label$text(/*text*/) {
+				if (arguments.length === 0) {
+					return this._text;
+				} else {
+					this._text = arguments[0];
+					this.element().text(this._text);
+				}
 			}
-		}
-	})
-	.complete();
-});
+		})
+		.complete();
+	});
+})(jQuery);
